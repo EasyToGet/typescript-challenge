04e41cad3a3c68553f9ffbeaab6a55a91de3bf1b
404NOTFOUND\yide1 <yide1986@gmail.com>
2023-12-26 00:57:21 +0800

    字符串反轉題目完成

--- src/stringReverse.ts
+++ src/stringReverse.ts
@@ -7,4 +7,10 @@
  */
 export function reverseString(str: string): string {
     // 在此實現函式
+    let arr = str.split('');
+    arr.reverse();
+    const reverseStr = arr.join('');
+    return reverseStr;
 }
+
+reverseString('hello');
