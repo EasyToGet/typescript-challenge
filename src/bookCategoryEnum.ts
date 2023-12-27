8d465c66553d40f6d552531e89bc429ace2ad683
404NOTFOUND\yide1 <yide1986@gmail.com>
2023-12-28 01:19:10 +0800

    書籍類別分類題目完成

--- src/bookCategoryEnum.ts
+++ src/bookCategoryEnum.ts
@@ -23,6 +23,12 @@ export enum BookCategory {
  * 輸出: 'Book category: Novel'
  */
 
-export function getBookCategory(category) {
+export function getBookCategory(category: BookCategory): string {
   // 在此實現函式
+  return `Book category: ${category}`;
 }
+
+const theBook: BookCategory = BookCategory.Novel;
+
+const outputBookInfo: string = getBookCategory(theBook);
+console.log(outputBookInfo);
