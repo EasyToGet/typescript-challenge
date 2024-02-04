bbe907c6897324182026a17ae7e28a84d669f821
404NOTFOUND\yide1 <yide1986@gmail.com>
2024-02-04 14:39:03 +0800

    完成 計算陣列總和：arraySum 題目

--- src/arraySum.ts
+++ src/arraySum.ts
@@ -14,4 +14,23 @@
  */
 export function arraySum(numbers: number[]): number {
     // 在此實現函式
-}
+    let sum: number = 0;
+
+    numbers.forEach(num => {
+        sum += num
+    });
+
+    return sum;
+};
+
+const input1: number[] = [1, 2, 3, 4, 5];
+const output1: number = arraySum(input1);
+console.log(output1);
+
+const input2: number[] = [-1, -2, -3];
+const output2: number = arraySum(input2);
+console.log(output2);
+
+const input3: number[] = [];
+const output3: number = arraySum(input3);
+console.log(output3);
